0163160fabdc592b0278e87f382bb5ec53c19053
viet009 <minhviet810business@gmail.com>
2022-06-07 22:37:07 +0700

    1st commit

---
+++ errs/errors.go
@@ -0,0 +1,49 @@
+package errs
+
+import "net/http"
+
+type AppError struct {
+	Code int `json:",omitempty"`
+	Message string `json:"message"`
+}
+
+func NewNotFoundError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewInternalError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewValidationError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusUnprocessableEntity,
+	}
+}
+
+func NewAuthenticationError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusUnauthorized,
+	}
+}
+
+func NewAuthorizationError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusForbidden,
+	}
+}
+
+func (e AppError) AsMessage() *AppError {
+	return &AppError{
+		Message: e.Message,
+	}
+}
